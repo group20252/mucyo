21bdf82b2dbeba29d4343b3add801e89112ea497
group20252 <fourgroup787@gmail.com>
2025-05-16 16:59:19 +0300

    Add files via upload

---
+++ siuu/siuu.ino
@@ -0,0 +1,14 @@
+int sensor=9;
+int buzzer=5;
+void setup(){
+pinMode(sensor,INPUT);
+pinMode(buzzer,OUTPUT);
+}
+void loop(){
+if(digitalRead(sensor)==HIGH){
+digitalWrite(buzzer,HIGH);
+}
+else{
+digitalWrite(buzzer,LOW);
+}
+}
